ec57cc8d7153a32e9935b20b29fe19d3006a1029
Gokulkrish300520 <gokul.jayakumar2005@gmail.com>
2025-09-02 14:29:50 +0530

    edited requirements.txt

--- server/start.sh
+++ server/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update apt repositories and install system libs required by WeasyPrint
-apt-get update && apt-get install -y libcairo2 libpango1.0-0 libgdk-pixbuf2.0-0 libffi-dev libgobject-2.0-0
+# apt-get update && apt-get install -y libcairo2 libpango1.0-0 libgdk-pixbuf2.0-0 libffi-dev libgobject-2.0-0
 
 # Collect static files
 python manage.py collectstatic --noinput
